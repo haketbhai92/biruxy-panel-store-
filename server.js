837b476ec94249d89610f3861c3691a5d4ccaa5f
haketbhai92 <haketbhai92@gmail.com>
2026-09-09 15:47:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,52 +2,105 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
 const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const orders = [];
+const users = [];
 
-app.post("/api/orders", (req, res) => {
-  const { orderId, product, price } = req.body;
 
-  if (!orderId || !product || !price) {
-    return res.status(400).json({
-      success: false,
-      message: "Invalid order"
+/* REGISTER */
+
+app.post("/api/register", (req, res) => {
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+
+        return res.status(400).json({
+            success:false,
+            message:"Username aur password required hai"
+        });
+
+    }
+
+    const exists = users.find(
+        user => user.username === username
+    );
+
+    if (exists) {
+
+        return res.json({
+            success:false,
+            message:"Username already registered hai"
+        });
+
+    }
+
+    users.push({
+        username,
+        password
     });
-  }
 
-  const order = {
-    orderId,
-    product,
-    price,
-    status: "pending",
-    key: null,
-    createdAt: new Date().toISOString()
-  };
+    console.log("NEW USER:", username);
 
-  orders.push(order);
+    res.json({
+        success:true,
+        message:"Registration successful"
+    });
 
-  console.log("NEW ORDER:", order);
-
-  res.json({
-    success: true,
-    message: "Order received",
-    order
-  });
 });
 
-app.get("/api/orders", (req, res) => {
-  res.json(orders);
+
+/* LOGIN */
+
+app.post("/api/login", (req, res) => {
+
+    const { username, password } = req.body;
+
+    const user = users.find(
+        u =>
+        u.username === username &&
+        u.password === password
+    );
+
+    if (!user) {
+
+        return res.json({
+            success:false,
+            message:"Wrong username ya password"
+        });
+
+    }
+
+    res.json({
+        success:true,
+        message:"Login successful"
+    });
+
 });
+
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+
+    res.sendFile(
+        path.join(
+            __dirname,
+            "public",
+            "index.html"
+        )
+    );
+
 });
 
+
 app.listen(PORT, () => {
-  console.log(`BIRUXY SHOP running on port ${PORT}`);
+
+    console.log(
+        "BIRUXY DIGITAL SHOP running on port " + PORT
+    );
+
 });
