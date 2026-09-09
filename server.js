380abb10b2fe3c7035bd8b698c3b3e6a0cbd7743
haketbhai92 <haketbhai92@gmail.com>
2026-09-09 15:29:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,111 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+const PORT = process.env.PORT || 10000;
+
+app.use(express.json());
+
+app.use(
+  express.static(
+    path.join(__dirname, "public")
+  )
+);
+
+
+const orders = [];
+
+
+app.post("/api/orders", (req,res)=>{
+
+const {
+orderId,
+product,
+price
+} = req.body;
+
+
+if(!orderId || !product || !price){
+
+return res.status(400).json({
+
+success:false,
+
+message:"Invalid order"
+
+});
+
+}
+
+
+const order = {
+
+orderId:orderId,
+
+product:product,
+
+price:price,
+
+status:"pending",
+
+key:null,
+
+createdAt:
+new Date().toISOString()
+
+};
+
+
+orders.push(order);
+
+
+console.log(
+"NEW ORDER:",
+order
+);
+
+
+res.json({
+
+success:true,
+
+message:"Order received",
+
+order:order
+
+});
+
+});
+
+
+app.get("/api/orders",(req,res)=>{
+
+res.json(orders);
+
+});
+
+
+app.get("*",(req,res)=>{
+
+res.sendFile(
+
+path.join(
+__dirname,
+"public",
+"index.html"
+)
+
+);
+
+});
+
+
+app.listen(PORT,()=>{
+
+console.log(
+"BIRUXY SHOP running on port " +
+PORT
+);
+
+});
